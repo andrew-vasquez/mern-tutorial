68fb1b8ecbb06d002948584010ab6d2f92a42b75
Andrew <103331877+Allials@users.noreply.github.com>
2025-09-08 10:31:35 -0500

    installed daisy ui

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,13 @@
+ 
+
+
+ const CreatePage = () => {
+    return(
+        <>
+        <h1>Create Page</h1>
+        </>
+    )
+ }
+
+ export default CreatePage;
+ 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+
+const HomePage = () => {
+  return (
+    <div className="bg-red-500 text-white">HomePage</div>
+  )
+}
+
+export default HomePage
